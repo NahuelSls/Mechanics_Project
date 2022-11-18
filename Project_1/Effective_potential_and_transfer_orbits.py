dcd67a4361bee643f9c06cdffd61a95c8824f7ef
nahuel.siles <nahuel.siles31@gmail.com>
2022-11-18 14:59:42 +0100

    Initial commit

---
+++ Project_1/Effective_potential_and_transfer_orbits.py
@@ -0,0 +1,12 @@
+import matplotlib.pylab as plt
+import numpy as np
+import math
+
+# Relevant constants, units S.I.
+# Gravity constant
+G = 6.67e-11
+
+# Earth and Sun constant mass
+m_s = 1.9885e30
+m_e = 5.972e24
+
